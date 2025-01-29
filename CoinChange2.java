42e175f36aba1cbfa314dcb6714e60d00be2bf37
Abhinash Mishra <abhimishrabhi2580@gmail.com>
2025-01-30 01:20:17 +0530

    dp day11

---
+++ CoinChange2.java
@@ -0,0 +1,26 @@
+package DP;
+
+public class CoinChange2 {
+    public static int coinChange(int[] coins, int amount) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        for (int i = 0; i <= coins.length; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j == 0) {
+                    dp[i][j] = 1;
+                } else if (i == 0) {
+                    dp[i][j] = 0;
+                } else if (coins[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[coins.length][amount];
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
